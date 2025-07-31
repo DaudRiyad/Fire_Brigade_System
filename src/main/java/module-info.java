826398b44b7c7003bd8a5a38492b28e046b990d7
DaudRiyad <135199529+DaudRiyad@users.noreply.github.com>
2025-07-31 20:58:00 +0600

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.fire_brigade_system {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.fire_brigade_system to javafx.fxml;
+    exports com.example.fire_brigade_system;
+}
